4146858f3e832114171916dc55b7f378aef6bf57
simon <simon.fluegel@ovgu.de>
2022-07-22 15:15:08 +0200

    add clever hans evaluation for tfkerasvis

--- unittests/TestCleverHans.py
+++ unittests/TestCleverHans.py
@@ -3,9 +3,13 @@ import unittest
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from Main import perform_attribution
+import torch
+from tf_keras_vis.utils.scores import CategoricalScore
+
+from Main import perform_attribution, perform_feature_visualization
 from methods import method_keys
 from toolsets import toolset_keys
+from unittests.TestTranslation import NoDropoutNet
 
 
 class TestCleverHans(unittest.TestCase):
@@ -14,7 +18,7 @@ class TestCleverHans(unittest.TestCase):
         (self.x_train, self.y_train), (self.x_test, self.y_test) = get_tf_data()
         self.tf_net = tf.keras.models.load_model(os.path.join('..', 'models', 'tf_clever_hans'))
 
-    def test_integrated_gradients(self):
+    def test_captum(self):
         n_samples = 8
         methods = [method_keys.INTEGRATED_GRADIENTS, method_keys.SALIENCY, method_keys.DEEP_LIFT,
                    method_keys.INPUT_X_GRADIENT, method_keys.FEATURE_ABLATION, method_keys.FEATURE_PERMUTATION]
@@ -34,7 +38,45 @@ class TestCleverHans(unittest.TestCase):
                 counter += 1
                 plt.xlabel(m)
                 plt.imshow(attr[i], cmap="gray")
-        plt.savefig('clever_hans_results.png')
+        plt.savefig('clever_hans_results_captum.png')
+        plt.show()
+
+    def test_tf_keras_vis(self):
+        torch_net = NoDropoutNet()
+        torch_net.load_state_dict(torch.load(os.path.join('..', 'models', 'torch_clever_hans.pth')))
+
+        n_samples = 8
+        torch_x = self.x_test[:n_samples]
+        torch_x = torch_x.astype(np.single)
+        torch_x = torch_x.reshape((torch_x.shape[0], torch_x.shape[3], torch_x.shape[1], torch_x.shape[2]))
+        torch_x = torch.from_numpy(torch_x)
+
+        methods = [method_keys.SALIENCY, method_keys.GRAD_CAM, method_keys.ACTIVATION_MAXIMIZATION]
+        figure = plt.figure(figsize=(5 * n_samples, 5 * (len(methods) + 1)))
+        counter = 1
+        for i in range(n_samples):
+            figure.add_subplot(len(methods) + 1, n_samples, counter)
+            counter += 1
+            plt.xlabel(self.y_test[i])
+            plt.imshow(self.x_test[i], cmap="gray")
+        for m in methods:
+            if m == method_keys.ACTIVATION_MAXIMIZATION:
+                attr = perform_feature_visualization(torch_net, m, plot=False, toolset=toolset_keys.TF_KERAS_VIS,
+                                                     dummy_input=torch_x,
+                                                     exec_args={'score': CategoricalScore(self.y_test[:8].tolist()),
+                                                                'seed_input': torch_x})
+            else:
+                attr = perform_attribution(torch_net, m, plot=False, toolset=toolset_keys.TF_KERAS_VIS,
+                                           dummy_input=torch_x,
+                                           exec_args={'score': CategoricalScore(self.y_test[:8].tolist()),
+                                                      'seed_input': torch_x})
+            print(type(attr))
+            for i in range(n_samples):
+                figure.add_subplot(len(methods) + 1, n_samples, counter)
+                counter += 1
+                plt.xlabel(m)
+                plt.imshow(attr[i], cmap="gray")
+        plt.savefig('clever_hans_results_tfkerasvis.png')
         plt.show()
 
 
