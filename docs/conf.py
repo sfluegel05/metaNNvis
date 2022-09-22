8ce06af0720111d18b89a9a312c2c68687b8136f
simon <simon.fluegel@ovgu.de>
2022-09-22 14:57:01 +0200

    update readthedocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Simon Flügel'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst-parser']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
